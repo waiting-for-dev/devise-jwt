77c3844c9710eebc8472d398d134831e155f3c64
Marc Busqué <marc@lamarciana.com>
2017-01-12 10:05:08 +0100

    Install and configure overcommit and engines

---
+++ .overcommit_gems.rb
@@ -0,0 +1,15 @@
+# frozen_string_literal: true
+
+source 'https://rubygems.org'
+
+gem 'overcommit', '~> 0.36'
+
+# Patch-level verification for Bundled apps
+gem 'bundler-audit', '~> 0.5'
+
+# Ruby code smell reporter
+gem 'reek', '~> 4.5'
+
+# Ruby code style checking
+gem 'rubocop', '~> 0.43'
+gem 'rubocop-rspec', '~> 1.7'
