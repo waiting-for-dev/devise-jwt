f67740d800c52a36eff61e13d6c7d2fee4300cbb
Pablo Crivella <pablocrivella@gmail.com>
2017-12-09 17:17:35 +0100

    Add Whitelist revocation strategy

--- lib/devise/jwt/revocation_strategies.rb
+++ lib/devise/jwt/revocation_strategies.rb
@@ -2,6 +2,7 @@
 
 require 'devise/jwt/revocation_strategies/jti_matcher'
 require 'devise/jwt/revocation_strategies/blacklist'
+require 'devise/jwt/revocation_strategies/whitelist'
 require 'devise/jwt/revocation_strategies/null'
 
 module Devise
