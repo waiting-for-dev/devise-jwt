347469dad1056f57b1df8915be637caeab8b2bca
Marc Busqué <marc@lamarciana.com>
2017-12-23 13:34:28 +0100

    Fix cops

--- spec/devise/jwt/revocation_strategies/null_spec.rb
+++ spec/devise/jwt/revocation_strategies/null_spec.rb
@@ -10,8 +10,4 @@ describe Devise::JWT::RevocationStrategies::Null do
       expect(strategy.jwt_revoked?(:whatever, :whatever)).to eq(false)
     end
   end
-
-  describe '#revoke_jwt(payload, user)' do
-    it('does nothing') {}
-  end
 end
