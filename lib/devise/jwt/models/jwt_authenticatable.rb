bdb5c46997234a931e207f1d782d6a006e5bda49
dokuo9 <dokuo.diary@gmail.com>
2018-06-22 17:51:47 +0900

    find resource by primary_key
    default is only use Model#id.
    So I replaced to Model#primary_key.

--- lib/devise/jwt/models/jwt_authenticatable.rb
+++ lib/devise/jwt/models/jwt_authenticatable.rb
@@ -17,7 +17,7 @@ module Devise
 
       included do
         def self.find_for_jwt_authentication(sub)
-          find_by(id: sub)
+          find_by(self.primary_key.to_sym => sub)
         end
       end
 
