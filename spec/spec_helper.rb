81c76e16b8974322bd52fe2905c95fc62fbd78d4
Marc Busqué <marc@lamarciana.com>
2019-11-08 18:03:41 +0100

    Fix static analysis warnings

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,17 +2,17 @@
 
 $LOAD_PATH.unshift File.expand_path('../lib', __dir__)
 require 'devise/jwt'
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-# require "active_job/railtie"
-require "active_record/railtie"
-require "action_controller/railtie"
-# require "action_mailer/railtie"
-require "action_view/railtie"
-# require "action_cable/engine"
-# require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'active_model/railtie'
+# require 'active_job/railtie'
+require 'active_record/railtie'
+require 'action_controller/railtie'
+# require 'action_mailer/railtie'
+require 'action_view/railtie'
+# require 'action_cable/engine'
+# require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 require 'rspec/rails'
 require 'pry-byebug'
