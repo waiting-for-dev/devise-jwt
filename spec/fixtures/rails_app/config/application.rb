5e238424f5dd21e55c73f5d914aaf2e8915408b0
Marc Busqué <marc@lamarciana.com>
2021-02-14 12:51:13 +0100

    Remove configuration from fixture app that was causing problems

--- spec/fixtures/rails_app/config/application.rb
+++ spec/fixtures/rails_app/config/application.rb
@@ -26,7 +26,5 @@ module RailsApp
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    Rails.application.config.active_record.sqlite3.represent_boolean_as_integer = true
   end
 end
