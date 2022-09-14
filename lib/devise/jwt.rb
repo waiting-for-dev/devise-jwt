26bfeafaa95e03526162a215e89d1b3ed1b887cf
Jouni Renfors <jouni@renfors.net>
2022-09-14 08:29:35 +0300

    Add support for decode_secret

--- lib/devise/jwt.rb
+++ lib/devise/jwt.rb
@@ -38,6 +38,10 @@ module Devise
             default: Warden::JWTAuth.config.secret,
             constructor: ->(value) { forward_to_warden(:secret, value) })
 
+    setting(:decoding_secret,
+            default: Warden::JWTAuth.config.decoding_secret,
+            constructor: ->(value) { forward_to_warden(:decoding_secret, value) })
+
     setting(:rotation_secret,
             default: Warden::JWTAuth.config.rotation_secret,
             constructor: ->(value) { forward_to_warden(:rotation_secret, value) })
